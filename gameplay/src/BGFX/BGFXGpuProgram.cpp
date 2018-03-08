439db743b394f4d875de1821c1af71e4203cb94d
fredakilla <fredakilla@gmail.com>
2018-03-08 05:41:40 +0100

    use default varying.def.sc

--- gameplay/src/BGFX/BGFXGpuProgram.cpp
+++ gameplay/src/BGFX/BGFXGpuProgram.cpp
@@ -29,9 +29,16 @@ BGFXGpuProgram::~BGFXGpuProgram()
 
 void BGFXGpuProgram::set(const char* vshPath, const char* fshPath, const char* defines)
 {
+    // use custom varying def file if exists or default "varying.def.sc"
+    std::string basename = FileSystem::getBaseName(vshPath);
+    std::string varyingFile = basename + ".io";
+    bool useCustomVaryingDef = FileSystem::fileExists(varyingFile.c_str());
+    if(!useCustomVaryingDef)
+        varyingFile = FileSystem::getDirectoryName(vshPath) + "varying.def.sc";
+
     // Compile shaders using brtshaderc library
-    const bgfx::Memory* memVsh = shaderc::compileShader(shaderc::ST_VERTEX, vshPath, defines, "res/shaders/varying.def.sc");
-    const bgfx::Memory* memFsh = shaderc::compileShader(shaderc::ST_FRAGMENT, fshPath, defines, "res/shaders/varying.def.sc");
+    const bgfx::Memory* memVsh = shaderc::compileShader(shaderc::ST_VERTEX, vshPath, defines, varyingFile.c_str());
+    const bgfx::Memory* memFsh = shaderc::compileShader(shaderc::ST_FRAGMENT, fshPath, defines, varyingFile.c_str());
 
     GP_ASSERT(memVsh);
     GP_ASSERT(memFsh);
@@ -42,6 +49,9 @@ void BGFXGpuProgram::set(const char* vshPath, const char* fshPath, const char* d
 
     // Create bgfx program.
     _program = bgfx::createProgram(_vsh, _fsh, true);
+
+    if(!bgfx::isValid(_program))
+        GP_ERROR("Error while creating bgfx program with shaders [%s], [%s], [%s].", vshPath, fshPath, defines);
     GP_ASSERT(bgfx::isValid(_program));
 
     // Query uniforms from shaders.
