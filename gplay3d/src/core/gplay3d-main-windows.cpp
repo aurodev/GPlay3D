fc16c6ff575e43d243930c836fa1a1c834baa1d4
fredakilla <fredakilla@gmail.com>
2018-05-08 05:05:40 +0200

    fix windows compil

--- gplay3d/src/core/gplay3d-main-windows.cpp
+++ gplay3d/src/core/gplay3d-main-windows.cpp
@@ -13,21 +13,33 @@ using namespace gameplay;
 extern int __app_argc;
 extern char** __app_argv;
 
+#include <SDL2/SDL.h>
+#undef main
+#define main SDL_main
+
 /**
  * Main entry point.
  */
 //extern "C" int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR cmdLine, int cmdShow)
 int main(int argc, char *argv[])
 {
-	__app_argc = argc;
-	__app_argv = argv;
+    __app_argc = argc;
+    __app_argv = argv;
 
     Game* game = Game::getInstance();
+
     Platform* platform = Platform::create(game);
     GP_ASSERT(platform);
-    int result = platform->enterMessagePump();
+
+    platform->start();
+    while(platform->processEvents())
+    {
+        platform->frame();
+    }
+    platform->stop();
+
     delete platform;
-    return result;
+    return 0;
 }
 
 #endif
